2ad9723dafc32c10e7c3a2afba43d67f743f7f09
Zarithya <zarithya.online@gmail.com>
2024-05-23 18:40:30 -0700

    Only try to connect when Warudo is open, show alert and OK icons

--- app.warudo.streamdeck.sdPlugin/index.js
+++ app.warudo.streamdeck.sdPlugin/index.js
@@ -25,14 +25,8 @@ let messages = {}; // context -> message
 let toggleStates = {}; // receiver name -> state
 
 const warudoEndpoint = 'ws://localhost:19069';
-// Always try to keep the websocket connection open
 let ws = null;
-// Always try to keep the websocket connection open
-setInterval(() => {
-    if (ws.readyState !== WebSocket.OPEN && ws.readyState !== WebSocket.CONNECTING) {
-        setupWebsocket();
-    }
-}, 1000);
+let _tryconnect = undefined;
 
 const setupWebsocket = () => {
     toggleStates = {};
@@ -43,7 +37,9 @@ const setupWebsocket = () => {
     }
     ws.onclose = function() {
         console.log('Disconnected from Warudo');
-        setupWebsocket();
+        if (_tryconnect) {
+            try_connect();
+        }
     }
     ws.onmessage = function(msg) {
         console.log('Message from Warudo', msg);
@@ -66,7 +62,25 @@ const setupWebsocket = () => {
         }
     }
 }
-setupWebsocket();
+
+const try_connect = () => {
+    _tryconnect = window.setTimeout(() => {
+        setupWebsocket();
+    }, 1000);
+}
+
+const cancel_try_connect = () => {
+    window.clearTimeout(_tryconnect);
+    _tryconnect = undefined;
+}
+
+const connecting = () => {
+    return (ws) && (ws.readyState == WebSocket.CONNECTING);
+}
+
+const connected = () => {
+    return (!connecting()) && (ws && (ws.readyState === WebSocket.OPEN));
+}
 
 const updateToggleStates = () => {
     // Find all toggle actions and update their state
@@ -128,15 +142,26 @@ actions.forEach(action => {
     action.onKeyDown(async jsn => {
         console.log('onKeyDown', jsn.context);
         getSettingsAndRun(jsn.context, () => {
-            const data = {
-                receiverName: receiverNames[jsn.context],
-            };
-            if (action === messageAction) {
-                data.message = messages[jsn.context] || '';
+            try {
+                const data = {
+                    receiverName: receiverNames[jsn.context],
+                };
+                if (action === messageAction) {
+                    data.message = messages[jsn.context] || '';
+                }
+
+                if (!connected()) {
+                    $SD.showAlert(jsn.context);
+                    return;
+                }
+
+                const payload = { action: actionType, data }
+                console.log('Sending payload', payload);
+                ws.send(JSON.stringify(payload));
+                $SD.showOk(jsn.context);
+            } catch (e) {
+                $SD.showAlert(jsn.context);
             }
-            const payload = { action: actionType, data }
-            console.log('Sending payload', payload)
-            ws.send(JSON.stringify(payload));
         });
     });
 
@@ -178,119 +203,15 @@ actions.forEach(action => {
 
 });
 
-/* STREAMDECK RELATED */
-
-// In this example, we're monitoring a couple of apps
-// that we've added to the manifest.json file
-// under the "monitoredApps" key
-// if one of the monitored apps is launched or terminated,
-// we'll update the key images and send the running apps list
-// to the property inspector
-
 $SD.onApplicationDidLaunch((jsn) => {
     const {event, payload} = jsn;
-    console.log('onApplicationDidLaunch', jsn, event, payload);
-    // our monitored app settings (in manifest.json) are case-sensitive
-    // so we need to capitalize the app name to match
-    const app = Utils.capitalize(Utils.getApplicationName(payload));
-    // there should be a corresponding image in the images folder
-    const img = `images/${payload.application}.png`;
-    // try to load it
-    Utils.loadImagePromise(img).then(results => {
-        MCONTEXTS.forEach(c => updateKeyImages(c, img));
-    });
-    // add the monitored app to our running apps list
-    if(!MPLUGINDATA.runningApps.includes(app)) {MPLUGINDATA.runningApps.push(app);};
-    // send the running apps list to the property inspector
-    MCONTEXTS.forEach(updateRunningApps);
+    console.log('Warudo is open, attempting to connect...', jsn, event, payload);
+    if (!connected()) {
+        try_connect();
+    }
 });
 
 $SD.onApplicationDidTerminate(({context,payload}) => {
-    console.log('onApplicationDidTerminate', payload, payload.application);
-    // our monitored app settings (in manifest.json) are case-sensitive
-    // so we need to capitalize the app name to match
-    const app = Utils.capitalize(Utils.getApplicationName(payload));
-    // remove the terminated app from our running apps list
-    MPLUGINDATA.runningApps = MPLUGINDATA.runningApps.filter(item => item !== app);
-    // there should be a corresponding image in the images folder
-    const img = `images/${payload.application}.png`;
-    // overlay our terminated image on top of the terminated app image
-    const arrImages = [img, 'images/terminated.png'];
-    // try to load them
-    Utils.loadImages(arrImages).then(images => {
-        // if successfully loaded, merge them
-        Utils.mergeImages(images).then(b64 => {
-            // and update the key images
-            MCONTEXTS.forEach(c => updateKeyImages(c, b64));
-            setTimeout(() => {
-                // after 1.5 seconds, reset the key images
-                MCONTEXTS.forEach(c => updateKeyImages(c, `images/default.png`));
-            }, 1500);
-        });
-    });
-    // update the running apps list in the property inspector
-    MCONTEXTS.forEach(updateRunningApps);
+    console.log('Warudo has quit, stopping any attempts at connecting...', payload, payload.application);
+    cancel_try_connect();
 });
-
-/** HELPERS */
-
-const updateRunningApps = (context) => {
-    console.log('updateRunningApps', MPLUGINDATA.runningApps);
-    // $SD.sendToPropertyInspector(context, {runningApps: MPLUGINDATA.runningApps});
-    sendToPropertyInspector(context, {runningApps: MPLUGINDATA.runningApps});
-};
-
-const updateKeyImages = (context, url) => {
-    // console.log('updateKeyImages', context);
-    $SD.setImage(context, url);
-};
-
-
-/** UTILITIES USED IN THIS DEMO */
-
-Utils.loadImagePromise = url =>
-    new Promise(resolve => {
-        const img = new Image();
-        img.onload = () => resolve({url, img, status: 'ok'});
-        img.onerror = () => resolve({url, img, status: 'error'});
-        img.src = url;
-    });
-
-Utils.loadImages = arrayOfUrls => Promise.all(arrayOfUrls.map(Utils.loadImagePromise));
-
-Utils.capitalize = str => {
-    return str.charAt(0).toUpperCase() + str.slice(1);
-};
-
-Utils.getApplicationName = (payload) => {
-    const isMac = $SD.appInfo.application.platform === 'mac';
-    if(payload && payload.application) {
-        return isMac ? payload.application.split('.').pop() : payload.application.split('.')[0];
-    }
-    return '';
-};
-
-Utils.mergeImages = (images = [], options = {width: 144, height: 144, format: 'image/png', quality: 1}, inCanvas) => new Promise(resolve => {
-    const canvas = inCanvas && inCanvas instanceof HTMLCanvasElement
-        ? inCanvas
-        : document.createElement('canvas');
-
-    const ctx = canvas.getContext('2d');
-    ctx.globalCompositeOperation = 'source-over';
-
-    resolve(Promise.all(images).then(images => {
-        canvas.width = options.width || 144;
-        canvas.height = options.height || 144;
-
-        // Draw images to canvas
-        images.forEach(image => {
-            ctx.globalAlpha = image.opacity ? image.opacity : 1;
-            return ctx.drawImage(image.img, image.x || 0, image.y || 0);
-        });
-
-        // Resolve all other data URIs sync
-        return canvas.toDataURL(options.format, options.quality);
-    }));
-});
-
-
